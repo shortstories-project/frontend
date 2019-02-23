65cdf10b94578c524dce2fd3bc090ed41f6fabf3
shashkovdanil <shashkovdanil@gmail.com>
2019-02-23 22:43:14 +0500

    Revert "PWA!"
    This reverts commit f811c66f25fc1e5ed3511c166e43325f906e4ba8.

--- components/Page.js
+++ components/Page.js
@@ -1,42 +1,72 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import PropTypes from 'prop-types'
 import Meta from './Meta'
 import GlobalStyles from '../constants/global-styles'
 import theme from '../constants/theme'
 
-class Page extends PureComponent {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-  }
+// const StyledPage = styled.div`
+//   ${props =>
+//     props.isSingleStory
+//       ? css`
+//           background-color: #ffffff;
+//           > div {
+//             height: auto;
+//           }
+//         `
+//       : css`
+//           position: relative;
+//           min-height: 100%;
+//           &::before {
+//             content: '';
+//             background-image: url('/static/images/topography.svg'),
+//               linear-gradient(20deg, rgb(69, 104, 220), rgb(176, 106, 179));
+//             background-size: 300px, auto;
+//             position: fixed;
+//             top: 0;
+//             left: 0;
+//             width: 100%;
+//             height: 100%;
+//             will-change: transform;
+//             z-index: -1;
+//           }
+//         `};
+//   color: ${props => props.theme.black};
+// `
 
-  componentDidMount() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then(() => {
-          console.log('service worker registration successful')
-        })
-        .catch(err => {
-          console.warn('service worker registration failed', err.message)
-        })
-    }
-  }
+// const Inner = styled.div`
+//   ${props =>
+//     props.withHeader
+//       ? css`
+//           max-width: ${props.theme.maxWidth};
+//           margin: 0 auto;
+//           padding: 2rem;
+//         `
+//       : css`
+//           height: 100vh;
+//           width: 100%;
+//           display: flex;
+//           align-items: center;
+//           justify-content: center;
+//         `};
+// `
 
-  render() {
-    const { children } = this.props
-    return (
-      <>
-        <ThemeProvider theme={theme}>
-          <>
-            <Meta />
-            {children}
-          </>
-        </ThemeProvider>
-        <GlobalStyles />
-      </>
-    )
-  }
+const Page = ({ children }) => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <>
+          <Meta />
+          {children}
+        </>
+      </ThemeProvider>
+      <GlobalStyles />
+    </>
+  )
+}
+
+Page.propTypes = {
+  children: PropTypes.node.isRequired,
 }
 
 export default Page
