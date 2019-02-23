cb527ac927fa2576a7afec660218d90216d83a73
shashkovdanil <shashkovdanil@gmail.com>
2019-02-23 22:49:42 +0500

    Add sw precache

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 const withCSS = require('@zeit/next-css')
 
-module.exports = withCSS()
+module.exports = withCSS({
+  webpack: config => {
+    config.plugins.push(
+      new SWPrecacheWebpackPlugin({
+        verbose: true,
+        staticFileGlobsIgnorePatterns: [/\.next\//],
+        runtimeCaching: [
+          {
+            handler: 'networkFirst',
+            urlPattern: /^https?.*/,
+          },
+        ],
+      })
+    )
+
+    return config
+  },
+})
